4c96978d81f2cb4878cbdaae582eaa1fc91e3fac
liaracf <imloara812@gmail.com>
2025-04-02 08:18:19 -0300

    atta

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\User;
+
+
 class AuthController extends Controller
 {
    public function store (Request $request) {
--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Models\User;
+
 
 
 Route::get('/user', function (Request $request) {
     return response()->json(['message'=>'API is working']);
 });
 
-Route::post('/register');
+Route::post('/register', [AuthController::class, 'store']);
