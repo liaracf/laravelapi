088e598af63ef051df562a3b4459745339170fb0
liaracf <imloara812@gmail.com>
2025-04-02 07:35:35 -0300

    att

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+   public function store (Request $request) {
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->save();
+        return response ()->json([
+           "user" => $user
+   
+        ]);
+      }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,14 +9,4 @@ Route::get('/user', function (Request $request) {
     return response()->json(['message'=>'API is working']);
 });
 
-Route::post('/register', function (Request $request) {
-     $user = new User();
-     $user->name = $request->name;
-     $user->email = $request->email;
-     $user->password = $request->password;
-     $user->save();
-     return response ()->json([
-        "user" => $user
-
-     ]);
-   });
+Route::post('/register');
