be8a9c6d6f905d3b247024664b2368066ccd5ac4
liaracf <imloara812@gmail.com>
2025-04-03 08:10:33 -0300

    abr3

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,6 +11,11 @@ class AuthController extends Controller
 {
    public function store (Request $request) {
         $user = new User();
+        if(user::where('email', $request->email)->exists()){
+         return response()->json([
+            "message" => "Email ja cadastrado!"
+         ],400 );
+      }
         $user->name = $request->name;
         $user->email = $request->email;
         $user->password = $request->password;
